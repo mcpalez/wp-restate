03eb22a1e6209b93a3cd568de58e5528fa60068c
PALEZ Michał <michal@mbp-palez.home>
2024-11-11 17:42:54 +0100

    changes at filter moda, cosmetic changes

--- themes/flatgroup/template-search/search-flat.php
+++ themes/flatgroup/template-search/search-flat.php
@@ -112,7 +112,7 @@ $flat_sizes = [
       <div class="advancedFilters__nav">
         <button id="advancedFilters__close">
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M21 21L12 12M12 12L3 3M12 12L21.0001 3M12 12L3 21.0001" stroke="#212121" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M15 19L8 12L15 5" stroke="white" stroke-width="2" stroke-linecap="square"/>
           </svg>
         </button>
         <p>Filtry</p>
